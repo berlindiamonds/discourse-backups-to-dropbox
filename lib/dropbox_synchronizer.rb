11a4fea2eaa4ea96294d2718533bab962b7c05bd
Jen <jvarelalijo@gmail.com>
2017-08-31 18:20:22 +0200

    fix chunked upload

--- lib/dropbox_synchronizer.rb
+++ lib/dropbox_synchronizer.rb
@@ -39,7 +39,7 @@ module DiscourseBackupToDropbox
     end
 
     def upload_unique_files(folder_name, dbx_files)
-      (backup - dbx_files).each do |f|
+      ([backup] - dbx_files).each do |f|
         if f.present?
           full_path  = f.path
           filename   = f.filename
@@ -53,7 +53,7 @@ module DiscourseBackupToDropbox
       if size < UPLOAD_MAX_SIZE then
         dbx.upload("/#{folder_name}/#{file_name}", "#{file_name}")
       else
-        backup.chunked_upload(folder_name, file_name, full_path)
+        chunked_upload(folder_name, file_name, full_path)
       end
     end
 
